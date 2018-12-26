fd32dde9d0b6abb5b4b2ce61888af1a293163ac2
Vishesh <vishesh2k6@gmail.com>
2018-12-26 02:45:23 -0600

    Create Palindrome Number.py

---
+++ Palindrome Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # Convert to string
+        string_x = str(x)
+        # Reverse string
+        reversed_x = ""
+        for i in string_x:
+            reversed_x = i + reversed_x
+        # Check if palindrom
+        if (reversed_x == string_x):
+            return True
+        else:
+            return False
