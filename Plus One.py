ddef758387f192f7c71de1616db0bd7dfad404be
Vishesh <vishesh2k6@gmail.com>
2018-12-29 05:29:00 -0600

    Create Plus One.py

---
+++ Plus One.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        number = 0
+        for i in range(len(digits)):
+            number += digits[i] * pow(10, len(digits) - 1 - i)
+        res = number + 1
+        res_to_list = [int(i) for i in str(res)]
+        return res_to_list
