7a8e3b8b24168124fa0530c100568036b23d7208
Vishesh <vishesh2k6@gmail.com>
2019-05-09 22:37:52 -0500

    Create Most Common Word.py

---
+++ Most Common Word.py
@@ -0,0 +1,24 @@
+#819
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        punc = "!?',;."
+        for c in punc: 
+            paragraph = paragraph.replace(c, " ")
+        d, res, count = {}, "", 0
+        for word in paragraph.lower().split():
+            if word in banned:
+                continue; # do nothing, continue with the loop
+            elif word in d:
+                d[word] += 1
+            else:
+                d[word] = 1
+            if d[word] > count:
+                count = d[word]
+                res = word
+        return res
+        
