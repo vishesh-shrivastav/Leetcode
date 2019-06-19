42679a8c83d805481847231c593f7fedff09bf06
Vishesh <vishesh2k6@gmail.com>
2019-06-18 23:43:22 -0400

    Create 92_Reverse_Linked_List_II.py

---
+++ 92_Reverse_Linked_List_II.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        if not head:
+            return None
+        
+        cur, prev = head, None
+        while m > 1:
+            prev = cur
+            cur = cur.next
+            m -= 1 
+            n -= 1
+
+        # The two pointers that will fix the final connections.
+        tail = cur 
+        con = prev
+
+        # Iteratively reverse the nodes until n becomes 0.
+        while n:
+            third = cur.next
+            cur.next = prev
+            prev = cur
+            cur = third
+            n -= 1
+
+        # Adjust the final connections as explained in the algorithm
+        if con:
+            con.next = prev
+        else:
+            head = prev
+        tail.next = cur
+        
+        return head
+        
