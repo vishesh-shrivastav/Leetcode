ddc6cecae97461a07cf038a482bb4a084b2a4eff
Vishesh <vishesh2k6@gmail.com>
2019-06-19 23:38:03 -0400

    Create 56_Merge_Intervals.py

---
+++ HZ/56_Merge_Intervals.py
@@ -0,0 +1,27 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        """
+        The idea is to sort the intervals by their starting points.  
+        Then, we take the first interval and compare its end with the next intervals starts. 
+        As long as they overlap, we update the end to be the max end of the overlapping intervals. 
+        Once we find a non overlapping interval, we can add the previous "extended" interval and start over.
+        Sorting takes O(n log(n)) and merging the intervals takes O(n). So, the resulting algorithm takes O(n log(n)).
+        """
+        
+        if len(intervals) <= 1:
+            return intervals
+        
+        intervals = sorted(intervals, key = lambda x: x[0])
+        res = []
+        new_interval = intervals[0]
+        res.append(new_interval)
+        
+        for interval in intervals:
+            if interval[0] <= new_interval[1]:
+                new_interval[1] = max(new_interval[1], interval[1])
+            else:
+                new_interval = interval
+                res.append(new_interval)
+        
+        return res
